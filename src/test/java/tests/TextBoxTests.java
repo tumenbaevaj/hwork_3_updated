22760641b78d1442ceaa71604f2cd992c119189b
tumenbaevaj <tumenbaevaj@gmail.com>
2026-04-20 12:15:36 +0600

    add selenide textbox test

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,40 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void setupSelenideEnv() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void successfulFillFormTest() {
+        open("https://demoqa.com/text-box");
+
+        executeJavaScript("""
+            document.getElementById('fixedban')?.remove();
+            document.querySelector('footer')?.remove();
+        """);
+
+        $("#userName").setValue("John Smith");
+        $("#userEmail").setValue("johnsmith@gmail.com");
+        $("#currentAddress").setValue("66 Karalaev street, Bishkek, KG");
+        $("#permanentAddress").setValue("3 Matrosov street, Bishkek, KG");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("John Smith"));
+        $("#output #email").shouldHave(text("johnsmith@gmail.com"));
+        $("#output #currentAddress").shouldHave(text("66 Karalaev street, Bishkek, KG"));
+        $("#output #permanentAddress").shouldHave(text("3 Matrosov street, Bishkek, KG"));
+    }
+}
