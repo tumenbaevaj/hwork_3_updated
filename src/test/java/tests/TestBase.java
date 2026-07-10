efd33d853917319ac89aadbfad1bbe47be75b6de
tumenbaevaj <tumenbaevaj@gmail.com>
2026-07-10 10:51:55 +0600

    enabled remote driver for Jenkins

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,7 +22,7 @@ public class TestBase {
     static void setupSelenideEnv() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @BeforeEach
